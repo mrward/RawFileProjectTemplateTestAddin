ca96e7704a46504f11547f38f607b7148eee91f7
Matt Ward <ward.matt@gmail.com>
2014-12-03 14:30:43 +0000

    Add test addin.

---
+++ package.cmd
@@ -0,0 +1,2 @@
+rem "C:\Program Files (x86)\MonoDevelop\bin\mdtool.exe" setup pack bin\MonoDevelop.PackageManagement.dll
+"C:\Program Files (x86)\Xamarin Studio\bin\mdtool.exe" setup pack bin\RawFileProjectTemplateTestAddin.dll
